b95362780a8fc928caef2fd43fdb66390e34f1cd
Muhammad Mohsin Akhlaq <167335203+MuhammadMohsin107@users.noreply.github.com>
2025-07-30 13:23:27 +0500

    Add files via upload

---
+++ navigation.jsx
@@ -0,0 +1,28 @@
+const Navigation = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="/images/image.jpg" alt="NIKE" />
+
+          <ul>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Location</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+export default Navigation;
